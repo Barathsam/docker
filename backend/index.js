d6ba0f3d58ceac026345cbf43dc810d906263e3b
Barath <barath@Baraths-Mac-mini.local>
2024-02-17 19:08:49 +0530

    s

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = 3001;
+import { Prisma } from '@prisma/client'
+// import prisma from '../../../prisma/index.ts'
+
 app.use(cors());
 app.use(express.json());
 app.use('*', (req, res, next) => {
@@ -9,6 +12,7 @@ app.use('*', (req, res, next) => {
     next()
 })
 app.post('/api/name', (req, res) => {
+    //  const getAllLoadingPoint = () => prisma.loadingPoint.findMany({})
     res.status(200).json(`got the name called ${req.body.name}`)
 })
 
